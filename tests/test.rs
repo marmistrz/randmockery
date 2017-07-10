ab673e5a13dcfd858a5d4aa90e804b66f263407c
Marcin Mielniczuk <marmistrz.dev@gmail.com>
2017-07-10 16:00:42 +0200

    Rename the project

--- tests/test.rs
+++ tests/test.rs
@@ -4,10 +4,13 @@ mod tests {
     fn integration() {
         use std::process::Command;
 
-        let status = Command::new("make").arg("getrandom-test").status().unwrap();
+        let status = Command::new("make")
+            .args(&["-C", "tests", "getrandom-test"])
+            .status()
+            .unwrap();
         assert!(status.success());
-        let status = Command::new("target/debug/ptrace-rust")
-            .arg("./getrandom-test")
+        let status = Command::new("target/debug/randmockery")
+            .arg("tests/getrandom-test")
             .status()
             .unwrap();
         assert!(status.success());
