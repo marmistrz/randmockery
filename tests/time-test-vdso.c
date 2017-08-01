cf31fe7574a2284119be47ef672669b397f6c785
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2017-08-01 15:43:49 +0200

    Add extra logging in time-test-vdso.c if the test fails.

--- tests/time-test-vdso.c
+++ tests/time-test-vdso.c
@@ -1,13 +1,16 @@
-#include <time.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <sys/syscall.h>
+#include <time.h>
+#include <unistd.h>
 
 int main() {
     for (int i = 0; i < 10; ++i) {
         // time(NULL) would use vDSO, testing real syscall
         time_t t = time(NULL);
-        if (t != i) return 1;
+        if (t != i) {
+            printf("Incorrect time! Expected: %d, got: %ld", i, t);
+            return 1;
+        }
     }
     return 0;
 }
