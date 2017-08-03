e12c074560ebcfbadd7a3f2731ca8a9fd23668a7
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2017-08-03 17:19:25 +0200

    Reduce code duplicatoin in test.rs

--- tests/test.rs
+++ tests/test.rs
@@ -6,11 +6,13 @@ extern crate libc;
 extern crate lazy_static;
 
 use randmockery::{intercept_syscalls, ptrace_setmem, spawn_child};
-use randmockery::syscall_override::OverrideRegistry;
+use randmockery::syscall_override::{OverrideRegistry, HandlerData};
 use randmockery::syscall_override::{getrandom, time};
 
 use std::process::Command;
 use std::sync::Mutex;
+
+use nix::unistd::Pid;
 
 lazy_static! {
     /// Any test that creates child processes must grab this mutex, regardless
@@ -24,33 +26,58 @@ macro_rules! get_mutex {
     }
 }
 
-fn test_getrandom<F>(command: &str, expected_exitcode: i8, mut gen: F)
-where
-    F: 'static + FnMut() -> u8,
+fn test_syscall<F, G>(
+    command: &str,
+    expected_exitcode: i8,
+    syscall_no: i64,
+    atenter: F,
+    atexit: G,
+    preload: Option<&str>,
+) where
+    F: 'static + FnMut(Pid) -> HandlerData,
+    G: 'static + FnMut(Pid, &HandlerData) -> (),
 {
-    let mut reg = OverrideRegistry::new();
-    reg.add(
-        ::libc::SYS_getrandom,
-        getrandom::atenter,
-        move |pid, data| ptrace_setmem(pid, data, &mut gen),
-    );
+    get_mutex!();
 
-    let pid = spawn_child(Command::new(command));
+    let mut reg = OverrideRegistry::new();
+    reg.add(syscall_no, atenter, atexit);
+
+    let mut cmd = Command::new(command);
+    if let Some(pr) = preload {
+        cmd.env("LD_PRELOAD", pr);
+    };
+    let pid = spawn_child(cmd);
+
     let exitcode = intercept_syscalls(pid, reg);
     assert_eq!(exitcode, expected_exitcode);
 }
 
-#[test]
-fn constant_gen() {
-    get_mutex!();
+fn test_getrandom<F>(command: &str, expected_exitcode: i8, mut gen: F)
+where
+    F: 'static + FnMut() -> u8,
+{
+    test_syscall(
+        command,
+        expected_exitcode,
+        ::libc::SYS_getrandom,
+        getrandom::atenter,
+        move |pid, data| ptrace_setmem(pid, data, &mut gen),
+        None,
+    )
+}
 
+#[test]
+fn constant_gen_ok() {
     test_getrandom("tests/getrandom-test", 0, || 0);
+}
+
+#[test]
+fn constant_gen_fail() {
     test_getrandom("tests/getrandom-test", 1, || 8);
 }
 
 #[test]
 fn random_gen() {
-    get_mutex!();
 
     use rand::{StdRng, SeedableRng, Rng};
     let mut rng = StdRng::from_seed(&[1, 2, 3, 4]);
@@ -61,8 +88,6 @@ fn random_gen() {
 
 #[test]
 fn intercept_forked_children() {
-    get_mutex!();
-
     use rand::{StdRng, SeedableRng, Rng};
     let mut rng = StdRng::from_seed(&[1, 2, 3, 4]);
     let gen = move || rng.gen::<u8>();
@@ -72,65 +97,53 @@ fn intercept_forked_children() {
 
 #[test]
 fn intercept_threads() {
-    get_mutex!();
-
     test_getrandom("tests/getrandom-thread-test", 0, || 0);
 }
 
 #[test]
 fn test_logical_time() {
-    get_mutex!();
-
-    let mut reg = OverrideRegistry::new();
-    reg.add(::libc::SYS_time, time::time_atenter, time::time_atexit);
-
-    let pid = spawn_child(Command::new("tests/time-test"));
-    let exitcode = intercept_syscalls(pid, reg);
-    assert_eq!(exitcode, 0);
+    test_syscall(
+        "tests/time-test",
+        0,
+        ::libc::SYS_time,
+        time::time_atenter,
+        time::time_atexit,
+        None,
+    );
 }
 
 #[test]
 fn test_logical_time_vdso() {
-    get_mutex!();
-
-    let mut reg = OverrideRegistry::new();
-    reg.add(::libc::SYS_time, time::time_atenter, time::time_atexit);
-
-    let mut cmd = Command::new("tests/time-test-vdso");
-    cmd.env("LD_PRELOAD", "tests/libmocktime.so");
-    let pid = spawn_child(cmd);
-    let exitcode = intercept_syscalls(pid, reg);
-    assert_eq!(exitcode, 0);
+    test_syscall(
+        "tests/time-test-vdso",
+        0,
+        ::libc::SYS_time,
+        time::time_atenter,
+        time::time_atexit,
+        Some("tests/time-test-vdso"),
+    );
 }
 
 #[test]
 fn test_clock_gettime() {
-    get_mutex!();
-
-    let mut reg = OverrideRegistry::new();
-    reg.add(
+    test_syscall(
+        "tests/clock_gettime-test",
+        0,
         ::libc::SYS_clock_gettime,
         time::clock_gettime_atenter,
         time::clock_gettime_atexit,
+        None,
     );
-
-    let pid = spawn_child(Command::new("tests/clock_gettime-test"));
-    let exitcode = intercept_syscalls(pid, reg);
-    assert_eq!(exitcode, 0);
 }
 
 #[test]
 fn test_gettimeofday() {
-    get_mutex!();
-
-    let mut reg = OverrideRegistry::new();
-    reg.add(
+    test_syscall(
+        "tests/gettimeofday-test",
+        0,
         ::libc::SYS_gettimeofday,
         time::gettimeofday_atenter,
         time::gettimeofday_atexit,
+        None,
     );
-
-    let pid = spawn_child(Command::new("tests/gettimeofday-test"));
-    let exitcode = intercept_syscalls(pid, reg);
-    assert_eq!(exitcode, 0);
 }
